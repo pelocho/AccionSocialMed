58fc76fa3584f107bded8e10150143a7f1d911ff
JuanMedinaMoya <0619706393@uma.es>
2019-12-13 13:21:48 +0100

    Clase TABLEVIEW
    muestra las actividades de la ong

---
+++ src/ong/ongMisActividadesTableView.java
@@ -0,0 +1,118 @@
+package ong;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+
+import login.loginView;
+import modelos.Solicitud;
+
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.Font;
+import javax.swing.SwingConstants;
+import java.awt.Color;
+
+public class ongMisActividadesTableView extends ongListaActividades {
+
+	private JPanel contentPane;
+	private JTable table;
+	private JFrame frmAccionsocialmed;
+	private Solicitud prueba;
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String user) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					ongMisActividadesTableView window = new ongMisActividadesTableView(user);
+					window.frmAccionsocialmed.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 * @throws Exception 
+	 */
+	public ongMisActividadesTableView(String user) throws Exception {
+		frmAccionsocialmed = new JFrame();
+		frmAccionsocialmed.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmAccionsocialmed.setTitle("AccionSocialMed");
+		frmAccionsocialmed.setBounds(100, 100, 530, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		frmAccionsocialmed.setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(10, 11, 399, 239);
+		contentPane.add(scrollPane);
+		int i = 1;
+		Solicitud prueba1 = new Solicitud(i);
+		
+		table = new JTable();
+		
+		table.setModel(new DefaultTableModel(
+			new Object[][] {
+				{prueba1.getSolicitante(), prueba1.getCorreoSolicitante(), prueba1.getActividad()},
+			},
+			new String[] {
+				"T\u00EDtulo", "Lugar", "Horas"
+			}
+		) {
+			Class[] columnTypes = new Class[] {
+				String.class, String.class, Integer.class
+			};
+			public Class getColumnClass(int columnIndex) {
+				return columnTypes[columnIndex];
+			}
+			boolean[] columnEditables = new boolean[] {
+				false, false, false
+			};
+			public boolean isCellEditable(int row, int column) {
+				return columnEditables[column];
+			}
+		});
+		table.getColumnModel().getColumn(0).setResizable(false);
+		table.getColumnModel().getColumn(0).setPreferredWidth(207);
+		table.getColumnModel().getColumn(1).setResizable(false);
+		table.getColumnModel().getColumn(1).setPreferredWidth(253);
+		table.getColumnModel().getColumn(2).setResizable(false);
+		scrollPane.setViewportView(table);
+		
+		JButton btnNewButton = new JButton("Ver detalles");
+		btnNewButton.setForeground(Color.BLACK);
+		btnNewButton.setFont(new Font("Tahoma", Font.PLAIN, 9));
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnNewButton.setBounds(419, 71, 85, 23);
+		contentPane.add(btnNewButton);
+		
+		JButton btnVolver = new JButton("Volver");
+		btnVolver.setFont(new Font("Tahoma", Font.PLAIN, 9));
+		btnVolver.setBounds(419, 139, 85, 23);
+		contentPane.add(btnVolver);
+		
+		btnVolver.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				ongMainView.main(null);
+				frmAccionsocialmed.dispose();
+			}
+		});
+	}
+}
