c74a34aa02616be1623151843865c9de82b69ff9
JuanMedinaMoya <0619706393@uma.es>
2019-12-16 20:53:36 +0100

    actuali

--- src/pantallasCompartidas/editarPerfilView.java
+++ src/pantallasCompartidas/editarPerfilView.java
@@ -21,9 +21,7 @@ import javax.swing.JCheckBox;
 public class editarPerfilView extends funcionesEditarPerfil{
 
 	private JFrame frmAccionsocialmed;
-	private JTextField lugar;
 	private JTextField telefono;
-	private JTextField textField;
 
 	/**
 	 * Launch the application.
@@ -55,7 +53,7 @@ public class editarPerfilView extends funcionesEditarPerfil{
 		frmAccionsocialmed = new JFrame();
 		frmAccionsocialmed.setIconImage(Toolkit.getDefaultToolkit().getImage(editarPerfilView.class.getResource("/imagenes/icono pequeno.png")));
 		frmAccionsocialmed.setTitle("AccionSocialMed");
-		frmAccionsocialmed.setBounds(100, 100, 330, 378);
+		frmAccionsocialmed.setBounds(100, 100, 330, 300);
 		frmAccionsocialmed.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frmAccionsocialmed.getContentPane().setLayout(null);
 		
@@ -64,85 +62,67 @@ public class editarPerfilView extends funcionesEditarPerfil{
 		btnVolver.setBounds(10, 11, 41, 23);
 		frmAccionsocialmed.getContentPane().add(btnVolver);
 		
-		JLabel lblLugarDePreferencia = new JLabel("Lugar de preferencia:");
-		lblLugarDePreferencia.setBounds(10, 108, 161, 23);
-		frmAccionsocialmed.getContentPane().add(lblLugarDePreferencia);
-		
 		JLabel lblTipo = new JLabel("Tipo(s):");
-		lblTipo.setBounds(10, 142, 49, 14);
+		lblTipo.setBounds(10, 70, 49, 14);
 		frmAccionsocialmed.getContentPane().add(lblTipo);
 		
 		JLabel lblrea = new JLabel("\u00C1rea(s):");
-		lblrea.setBounds(153, 142, 65, 14);
+		lblrea.setBounds(153, 70, 65, 14);
 		frmAccionsocialmed.getContentPane().add(lblrea);
 		
 		JLabel lblCambiarTelfono = new JLabel("Cambiar tel\u00E9fono:");
-		lblCambiarTelfono.setBounds(10, 83, 115, 14);
+		lblCambiarTelfono.setBounds(10, 45, 115, 14);
 		frmAccionsocialmed.getContentPane().add(lblCambiarTelfono);
 		
-		lugar = new JTextField();
-		lugar.setBounds(147, 109, 149, 20);
-		frmAccionsocialmed.getContentPane().add(lugar);
-		lugar.setColumns(10);
-		
 		JCheckBox chckbxSalud = new JCheckBox("Salud");
-		chckbxSalud.setBounds(10, 163, 60, 23);
+		chckbxSalud.setBounds(10, 91, 60, 23);
 		frmAccionsocialmed.getContentPane().add(chckbxSalud);
 		
 		JCheckBox chckbxEducacin = new JCheckBox("Educaci\u00F3n");
-		chckbxEducacin.setBounds(10, 189, 94, 23);
+		chckbxEducacin.setBounds(10, 117, 94, 23);
 		frmAccionsocialmed.getContentPane().add(chckbxEducacin);
 		
 		JCheckBox chckbxIntegracin = new JCheckBox("Integraci\u00F3n");
-		chckbxIntegracin.setBounds(10, 215, 94, 23);
+		chckbxIntegracin.setBounds(10, 142, 94, 23);
 		frmAccionsocialmed.getContentPane().add(chckbxIntegracin);
 		
 		JCheckBox chckbxSaludSexual = new JCheckBox("Salud sexual");
-		chckbxSaludSexual.setBounds(10, 241, 109, 23);
+		chckbxSaludSexual.setBounds(10, 168, 109, 23);
 		frmAccionsocialmed.getContentPane().add(chckbxSaludSexual);
 		
 		JCheckBox chckbxNuevo = new JCheckBox("Nuevo");
-		chckbxNuevo.setBounds(10, 267, 94, 23);
+		chckbxNuevo.setBounds(10, 194, 94, 23);
 		frmAccionsocialmed.getContentPane().add(chckbxNuevo);
 		
 		JCheckBox chckbxAncianos = new JCheckBox("Ancianos");
-		chckbxAncianos.setBounds(153, 163, 94, 23);
+		chckbxAncianos.setBounds(153, 91, 94, 23);
 		frmAccionsocialmed.getContentPane().add(chckbxAncianos);
 		
 		JCheckBox chckbxInmigrantes = new JCheckBox("Inmigrantes");
-		chckbxInmigrantes.setBounds(153, 189, 109, 23);
+		chckbxInmigrantes.setBounds(153, 117, 109, 23);
 		frmAccionsocialmed.getContentPane().add(chckbxInmigrantes);
 		
 		JCheckBox chckbxNios = new JCheckBox("Ni\u00F1os");
-		chckbxNios.setBounds(153, 215, 94, 23);
+		chckbxNios.setBounds(153, 142, 94, 23);
 		frmAccionsocialmed.getContentPane().add(chckbxNios);
 		
 		JCheckBox chckbxNecesitados = new JCheckBox("Necesitados");
-		chckbxNecesitados.setBounds(153, 241, 109, 23);
+		chckbxNecesitados.setBounds(153, 168, 109, 23);
 		frmAccionsocialmed.getContentPane().add(chckbxNecesitados);
 		
 		JCheckBox chckbxAdictos = new JCheckBox("Adictos");
-		chckbxAdictos.setBounds(153, 267, 94, 23);
+		chckbxAdictos.setBounds(153, 194, 94, 23);
 		frmAccionsocialmed.getContentPane().add(chckbxAdictos);
 		
 		JButton btnGuardarCambios = new JButton("Guardar cambios");
 		btnGuardarCambios.setBackground(Color.LIGHT_GRAY);
-		btnGuardarCambios.setBounds(10, 304, 149, 23);
+		btnGuardarCambios.setBounds(10, 224, 149, 23);
 		frmAccionsocialmed.getContentPane().add(btnGuardarCambios);
 		
 		telefono = new JTextField();
 		telefono.setColumns(10);
-		telefono.setBounds(147, 77, 149, 20);
+		telefono.setBounds(147, 42, 149, 20);
 		frmAccionsocialmed.getContentPane().add(telefono);
-		
-		JLabel lblCambiarContrasea = new JLabel("Cambiar contrase\u00F1a:");
-		lblCambiarContrasea.setBounds(10, 45, 149, 23);
-		frmAccionsocialmed.getContentPane().add(lblCambiarContrasea);
-		
-		textField = new JTextField();
-		textField.setColumns(10);
-		textField.setBounds(147, 46, 149, 20);
-		frmAccionsocialmed.getContentPane().add(textField);
 		
 		btnVolver.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -176,7 +156,7 @@ public class editarPerfilView extends funcionesEditarPerfil{
 				
 				//Preferencias tipo
 				if(chckbxSalud.isSelected()) {
-					bd.insert("INSERT INTO eef_primera_iteracion.alumno_tipo_preferencia (Tipo, DNI) VALUES("+1+", '"+dni+"');");
+					bd.insert("INSERT INTO eef_primera_iteracion._tipo_preferencia (Tipo, DNI) VALUES("+1+", '"+dni+"');");
 				}
 				if(chckbxEducacin.isSelected()) {
 					bd.insert("INSERT INTO eef_primera_iteracion.alumno_tipo_preferencia (Tipo, DNI) VALUES("+2+", '"+dni+"');");
